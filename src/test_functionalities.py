c6e253c8cd5af8ca12444aecc4b2ae7c81d24814
debajyoti <debajyoti94@gmail.com>
2021-01-08 20:06:53 +0530

    Defined unit test cases based on feature engineering. Need to add code.

---
+++ src/test_functionalities.py
@@ -0,0 +1,25 @@
+''' In this code we will add unit test functionalities using pytest module'''
+
+import pickle
+from config import titanic_train_set, titanic_test_set,\
+                    file_delimiter
+
+class TestFunctionality:
+
+    def test_file_delimiter(self):
+        return
+
+    def test_dataset_shape(self):
+        return
+
+    def test_label_encoding(self):
+        return
+
+    def test_dump_pickled_file(self):
+        return
+
+    def test_plot_null_values(self):
+        return
+
+    def test_cleaning_data(self):
+        return
