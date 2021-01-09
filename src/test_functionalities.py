3de8a27847b9628fd787ad7182417a8eb326f75a
debajyoti <debajyoti94@gmail.com>
2021-01-09 14:12:09 +0530

    Added unit test cases for doing data sanity checks.

--- src/test_functionalities.py
+++ src/test_functionalities.py
@@ -3,23 +3,40 @@
 import pickle
 from config import titanic_train_set, titanic_test_set,\
                     file_delimiter
+import pandas as pd
+import csv
+import os
 
 class TestFunctionality:
 
     def test_file_delimiter(self):
-        return
+        '''
+        check if the file delimiter matches with the one mentioned
+        in config file
+        :return:
+        '''
+        with open(titanic_train_set, 'r') as csv_file:
+            file_contents = csv.Sniffer().sniff(csv_file.readline())
+
+            assert True if (file_contents.delimiter == file_delimiter) else False
 
     def test_dataset_shape(self):
-        return
-
-    def test_label_encoding(self):
-        return
-
-    def test_dump_pickled_file(self):
-        return
+        '''
+        checking if the dataset shape matches when file is opened
+        :return:
+        '''
+        train_dataset = pd.read_csv(titanic_train_set, delimiter=file_delimiter)
+        assert True if train_dataset.shape == (891,12) else False
 
     def test_plot_null_values(self):
-        return
+        '''
+        check if the plot is created in the location
+        :return:
+        '''
+        assert True if os.path.isfile(titanic_train_set) and\
+                       os.path.isfile(titanic_test_set) \
+                       else False
+
 
     def test_cleaning_data(self):
         return
