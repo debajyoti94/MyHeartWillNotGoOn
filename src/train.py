4b0f97698041c73030bb1e81ef3e27b0580c9c86
debajyoti <debajyoti94@gmail.com>
2021-01-10 16:17:30 +0530

    Added code for inference stage. Still some work is remaining.

--- src/train.py
+++ src/train.py
@@ -16,7 +16,7 @@ import pandas as pd
 import argparse
 
 # this function is dedicated to putting things together
-def run(dataset, fold):
+def train_model(dataset, fold):
     '''
     This is where we train the model using the dataset
     :param dataset:
@@ -31,6 +31,8 @@ def run(dataset, fold):
     # dropping the output feature and kfold feature from X_train
     X_train = dataset_train.drop([config.output_feature, 'kfold'],
                                  axis=1).values
+    # print(X_train.head())
+    # print(X_train.shape)
 
     # keeping only target feature for y_train
     y_train =dataset_train[config.output_feature].values
@@ -59,19 +61,33 @@ def run(dataset, fold):
     with open(config.baseline_model+'_'+str(fold), 'wb') as pickle_handle:
         pickle.dump(lr,pickle_handle)
 
+def inference_time(model, test_data):
+    '''
+    This is where we apply obtain performance on the test set
+    :param model:
+    :param test_data:
+    :return:
+    '''
+    print(test_data.isnull().any())
+    X_test = test_data.values
+
+    for input in X_test:
+
+        print(input)
+        preds = model.predict(input)
+        print(preds)
+    # accuracy = metrics.accuracy_score(y_test, preds)
+    # p,r,f1_score,support = metrics.precision_recall_fscore_support(y_test,
+    #                                                                preds)
+    #
+    # print("Test set accuracy = {}".format(accuracy))
+    # print("---Test set---\nAccuracy={}\nPrecision={}\nRecall={}\nF1={}".
+    #       format(accuracy, p, r, f1_score))
 
 
 if __name__ == "__main__":
 
-    # first we gather the data
-    dataset = pd.read_csv(config.titanic_train_set,
-                          delimiter=config.file_delimiter)
-
-    # create object of FeatureEngineering class
-    fr_obj = FeatureEngineering()
-    cleaned_data = fr_obj.cleaning_data(dataset,dataset_type="TRAIN")
-
-    # next we inititalize the arguments using argparse
+    # we inititalize the arguments using argparse
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--train", type=str,
@@ -83,10 +99,33 @@ if __name__ == "__main__":
 
     # then we call the run() function
     if args.train == 'skfold':
-        run(cleaned_data, 0)
-        run(cleaned_data, 1)
-        run(cleaned_data, 2)
-        run(cleaned_data, 3)
-        run(cleaned_data, 4)
-    if args.test:
+        # first we gather the data
+        dataset = pd.read_csv(config.titanic_train_set,
+                              delimiter=config.file_delimiter)
+
+        # create object of FeatureEngineering class
+        fr_obj = FeatureEngineering()
+        cleaned_data = fr_obj.cleaning_data(dataset, dataset_type="TRAIN")
+
+        # train the model
+        train_model(cleaned_data, 0)
+        train_model(cleaned_data, 1)
+        train_model(cleaned_data, 2)
+        train_model(cleaned_data, 3)
+        train_model(cleaned_data, 4)
+    if args.test == 'inference':
+        # first we gather the data
+        dataset = pd.read_csv(config.titanic_test_set,
+                              delimiter=config.file_delimiter)
+
+        # create object of FeatureEngineering class
+        fr_obj = FeatureEngineering()
+        cleaned_data = fr_obj.cleaning_data(dataset, dataset_type="TEST")
+
+        # load model
+        with open(config.best_baseline_model, 'rb') as pickle_handle:
+            inference_model = pickle.load(pickle_handle)
+
+        inference_time(inference_model, cleaned_data)
+
         print("Code not ready yet for model inference.")
